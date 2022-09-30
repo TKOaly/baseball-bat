aced597c3e9244f9884f349040455dd7c4052919
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2022-09-30 05:38:23 +0300

    Use table view for debt center listing

--- frontend/views/admin/debt-centers-listing.tsx
+++ frontend/views/admin/debt-centers-listing.tsx
@@ -1,8 +1,10 @@
 import { ListView } from '../../components/list-view'
 import { tw } from '../../tailwind'
 import { useLocation } from 'wouter';
+import { format, parseISO } from 'date-fns'
 import { useGetDebtCentersQuery } from '../../api/debt-centers'
 import { Button, SecondaryButton } from '../../components/button'
+import { TableView } from '../../components/table-view';
 
 export const DebtCentersListing = () => {
   const { data, isLoading } = useGetDebtCentersQuery(null)
@@ -11,28 +13,40 @@ export const DebtCentersListing = () => {
 
   const debtCenters = data && !isLoading ? data : [];
 
+  const rows = (data ?? []).map((center) => ({ key: center.id, ...center }))
+
   return (
     <>
       <h1 className="text-2xl mb-5 mt-10">Debt Centers</h1>
       <p className="text-gray-800 mb-7 text-md">
         Debt centers are organizational groupings of debts, usually corresponding to events or other batches of sales.
-        Here you can view and create new debt centers.
+        Here you can view and create new debt centers. <br />
       </p>
-      <ListView
-        actions={
-          <>
-            <Button onClick={() => setLocation('/admin/debt-centers/create')}>Create</Button>
-            <SecondaryButton onClick={() => setLocation('/admin/debt-centers/create-from-event')}>Create from Event</SecondaryButton>
-          </>
-        }
-        items={debtCenters.map((center) => ({
-          key: center.id,
-          title: center.name,
-          description: center.description,
-          label: center.url,
-          badges: [],
-        }))}
-        onSelected={(item) => setLocation(`/admin/debt-centers/${item.key}`)}
+      <TableView
+        rows={rows}
+        onRowClick={(item) => setLocation(`/admin/debt-centers/${item.id}`)}
+        columns={[
+          { name: 'Title', getValue: 'name' },
+          {
+            name: 'Created',
+            getValue: 'createdAt',
+            render: (value) => format(parseISO(value), 'dd.MM.yyyy'),
+          },
+          {
+            name: 'Paid percentage',
+            getValue: (row) => row.paidCount / row.debtCount,
+            render: (value) => (
+              <div className="w-full">
+                <div className="text-xs">{(value * 100).toFixed(0)}%</div>
+                <div className="h-1.5 bg-gray-200 w-full rounded-full overflow-hidden">
+                  <div className="h-full bg-green-400" style={{ width: `${(value * 100).toFixed()}%` }} />
+                </div>
+              </div>
+            ),
+          },
+          { name: 'Paid', getValue: 'paidCount', align: 'right' },
+          { name: 'Debts Count', getValue: 'debtCount', align: 'right' },
+        ]}
       />
     </>
   );
