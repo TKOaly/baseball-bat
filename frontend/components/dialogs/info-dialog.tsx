cc7a7e3002a09cbd4feb95b6712a8c607d99124e
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2022-10-26 22:30:47 +0300

    Implement generic information dialog

---
+++ frontend/components/dialogs/info-dialog.tsx
@@ -0,0 +1,16 @@
+import { DialogBase, DialogContent, DialogFooter, DialogHeader } from '../../components/dialog'
+import { Button } from '../../components/button'
+
+export const InfoDialog = ({ onClose, title, content }) => {
+  return (
+    <DialogBase onClose={() => onClose()}>
+      <DialogHeader>{title}</DialogHeader>
+      <DialogContent>
+        {content}
+      </DialogContent>
+      <DialogFooter>
+        <Button onClick={() => onClose()}>Close</Button>
+      </DialogFooter>
+    </DialogBase>
+  )
+}
