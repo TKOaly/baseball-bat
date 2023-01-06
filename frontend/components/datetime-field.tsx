90788df0f8ba80df756dcf82bbfac5232fe00a39
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-01-06 09:33:09 +0200

    Implement DateField using floating-ui to allow the drawer to escape parent containers with overflow set to something other than visible.

--- frontend/components/datetime-field.tsx
+++ frontend/components/datetime-field.tsx
@@ -5,11 +5,12 @@ import { parse, format, isMatch } from 'date-fns';
 import { useOutsideEventListener } from '../hooks/useOutsideEventListener';
 
 import 'react-day-picker/dist/style.css';
+import { autoUpdate, FloatingPortal, useClick, useDismiss, useFloating, useInteractions } from '@floating-ui/react-dom-interactions';
 
 export const DateField = ({ value, allowEmpty = true, onChange, format: formatString = 'dd.MM.yyyy', ...props }) => {
   const [calendarOpen, setCalendarOpen] = useState(false);
   const ref = useRef();
-  useOutsideEventListener(ref, 'click', calendarOpen, () => setCalendarOpen(false));
+  // useOutsideEventListener(ref, 'click', calendarOpen, () => setCalendarOpen(false));
 
   const [displayValue, setDisplayValue] = useState('');
 
@@ -28,9 +29,24 @@ export const DateField = ({ value, allowEmpty = true, onChange, format: formatSt
     resetDisplayValue();
   }, [value, resetDisplayValue]);
 
+  const { x, y, reference, floating, strategy, context } = useFloating({
+    placement: 'bottom',
+    open: calendarOpen,
+    onOpenChange: setCalendarOpen,
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    useClick(context),
+    useDismiss(context),
+  ]);
+
   return (
     <div className="flex flex-col items-center relative" ref={ref}>
-      <div className="relative w-full">
+      <div
+        ref={reference}
+        className="relative w-full"
+        {...getReferenceProps()}
+      >
         <input
           type="text"
           className={`
@@ -71,17 +87,28 @@ export const DateField = ({ value, allowEmpty = true, onChange, format: formatSt
           <Calendar className={`cursor-pointer ${calendarOpen ? 'text-blue-500 hover:text-blue-400' : 'text-gray-400 hover:text-gray-500'}`} onClick={() => setCalendarOpen(!calendarOpen)} />
         </div>
       </div>
-      {calendarOpen && (
-        <div className="bg-white rounded-b-md absolute top-full z-10 border shadow-xl border-gray-200 mt-[-1px]">
-          <DayPicker
-            mode="single"
-            modifiersClassNames={{ selected: 'bg-blue-500' }}
-            onSelect={(day) => {
-              onChange({ target: { id: props.id, name: props.name, value: format(day, formatString) } });
+      <FloatingPortal>
+        {calendarOpen && (
+          <div
+            ref={floating}
+            style={{
+              position: strategy,
+              top: y ?? 0,
+              left: x ?? 0,
             }}
-          />
-        </div>
-      )}
+            className="bg-white rounded-b-md absolute top-full z-10 border shadow-xl border-gray-200 mt-[-1px]"
+            {...getFloatingProps()}
+          >
+            <DayPicker
+              mode="single"
+              modifiersClassNames={{ selected: 'bg-blue-500' }}
+              onSelect={(day) => {
+                onChange({ target: { id: props.id, name: props.name, value: format(day, formatString) } });
+              }}
+            />
+          </div>
+        )}
+      </FloatingPortal>
     </div>
   );
 };
