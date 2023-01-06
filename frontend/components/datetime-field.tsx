694f163cc3f7342cfabaa82665d0b2b29b4319cc
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-01-06 09:18:43 +0200

    Fix DateField crashing when a date has been partially typed

--- frontend/components/datetime-field.tsx
+++ frontend/components/datetime-field.tsx
@@ -1,15 +1,32 @@
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import { Calendar } from 'react-feather';
-import { parse, format } from 'date-fns';
+import { parse, format, isMatch } from 'date-fns';
 import { useOutsideEventListener } from '../hooks/useOutsideEventListener';
 
 import 'react-day-picker/dist/style.css';
 
-export const DateField = ({ value, onChange, format: formatString = 'dd.MM.yyyy', ...props }) => {
+export const DateField = ({ value, allowEmpty = true, onChange, format: formatString = 'dd.MM.yyyy', ...props }) => {
   const [calendarOpen, setCalendarOpen] = useState(false);
   const ref = useRef();
   useOutsideEventListener(ref, 'click', calendarOpen, () => setCalendarOpen(false));
+
+  const [displayValue, setDisplayValue] = useState('');
+
+  const resetDisplayValue = useCallback(() => {
+    console.log('RESET');
+
+    try {
+      const date = parse(value, formatString, new Date());
+      setDisplayValue(format(date, 'dd.MM.yyyy'));
+    } catch (e) {
+      setDisplayValue('');
+    }
+  }, [value, formatString, setDisplayValue]);
+
+  useEffect(() => {
+    resetDisplayValue();
+  }, [value, resetDisplayValue]);
 
   return (
     <div className="flex flex-col items-center relative" ref={ref}>
@@ -28,9 +45,23 @@ export const DateField = ({ value, onChange, format: formatString = 'dd.MM.yyyy'
             px-3
             border
           `}
-          value={value ? format(parse(value, formatString, new Date()), 'dd.MM.yyyy') : ''}
-          onChange={onChange}
+          value={displayValue}
+          onChange={(evt) => {
+            setDisplayValue(evt.target.value);
+
+            if (isMatch(evt.target.value, 'dd.MM.yyyy')) {
+              onChange(evt);
+            }
+
+            if (evt.target.value === '' && allowEmpty) {
+              onChange(evt);
+            }
+          }}
           {...props}
+          onBlur={(evt) => {
+            resetDisplayValue();
+            props?.onBlur?.(evt);
+          }}
         />
         <div className="absolute right-0 top-0 flex items-center mr-2 pt-1 bottom-0">
           <Calendar className={`cursor-pointer ${calendarOpen ? 'text-blue-500 hover:text-blue-400' : 'text-gray-400 hover:text-gray-500'}`} onClick={() => setCalendarOpen(!calendarOpen)} />
