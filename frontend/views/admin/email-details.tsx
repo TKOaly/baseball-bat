aef44c9b62e6b0ceb1de1a0401c1c6e27e3ab730
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-06-03 15:45:19 +0300

    List debts with which an email is associated on the email details page

--- frontend/views/admin/email-details.tsx
+++ frontend/views/admin/email-details.tsx
@@ -1,11 +1,14 @@
+import { useGetDebtsByEmailQuery } from '../../api/debt';
 import { useGetEmailQuery } from '../../api/email';
 import { useGetPayerByEmailQuery } from '../../api/payers';
 import { Breadcrumbs } from '../../components/breadcrumbs';
-import { Page, Header, Title, Section, TextField, SectionContent, LinkField, BadgeField } from '../../components/resource-page/resource-page';
+import { ResourceLink } from '../../components/resource-link';
+import { Page, Header, Title, Section, TextField, SectionContent, LinkField, BadgeField, Field } from '../../components/resource-page/resource-page';
 
 export const EmailDetails = ({ params }: { params: { id: string } }) => {
   const { data: email } = useGetEmailQuery(params.id);
   const { data: payer } = useGetPayerByEmailQuery(email?.recipient, { skip: !email });
+  const { data: debts } = useGetDebtsByEmailQuery(params.id);
 
   if (!email) {
     return <div>Loading...</div>;
@@ -39,6 +42,13 @@ export const EmailDetails = ({ params }: { params: { id: string } }) => {
         <LinkField label="Payer" text={payer?.name} to={`/admin/payers/${payer?.id?.value}`} />
         <TextField label="Used Template" value={email.template} />
         <BadgeField label="Status" color="gray" text={status} />
+        { debts && debts.length > 0 && (
+          <Field label="Debts">
+            {debts.map((debt) => (
+              <ResourceLink type="debt" id={debt.id} />
+            ))}
+          </Field>
+        )}
       </Section>
       <Section title="Preview">
         <SectionContent>
