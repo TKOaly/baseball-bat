a40a0c868813ca36318f51d83b828272c563468b
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-06-03 15:38:38 +0300

    Separate TableView listing emails into a separate EmailList component

--- frontend/views/admin/emails-listing.tsx
+++ frontend/views/admin/emails-listing.tsx
@@ -2,6 +2,7 @@ import { useGetEmailsQuery, useSendEmailsMutation } from '../../api/email';
 import { TableView } from '../../components/table-view';
 import { useLocation } from 'wouter';
 import { format } from 'date-fns';
+import { EmailList } from '../../components/email-list';
 
 export const EmailsListing = () => {
   const [, setLocation] = useLocation();
@@ -12,50 +13,7 @@ export const EmailsListing = () => {
     <>
       <h1 className="text-2xl mt-10 mb-5">Emails</h1>
 
-      <TableView
-        rows={(emails ?? []).map(e => ({ ...e, key: e.id })) ?? []}
-        columns={[
-          {
-            name: 'Recipient',
-            getValue: 'recipient',
-          },
-          {
-            name: 'Subject',
-            getValue: 'subject',
-          },
-          {
-            name: 'Created',
-            getValue: 'createdAt',
-            render: (value) => value && format(new Date(value), 'dd.MM.yyyy'),
-          },
-          {
-            name: 'Sent',
-            getValue: 'sentAt',
-            render: (value) => value && format(new Date(value), 'dd.MM.yyyy'),
-          },
-          {
-            name: 'Status',
-            getValue: (row) => {
-              if (row.draft) {
-                return 'Draft';
-              }
-
-              return 'Pending';
-            },
-          },
-        ]}
-        selectable={true}
-        actions={[
-          {
-            key: 'send',
-            text: 'Send',
-            onSelect: async (emails) => {
-              await sendEmails(emails.map(e => e.id));
-            },
-          },
-        ]}
-        onRowClick={(row) => setLocation(`/admin/emails/${row.id}`)}
-      />
+      <EmailList emails={emails ?? []} />
     </>
   );
 };
