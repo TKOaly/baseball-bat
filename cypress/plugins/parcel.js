76d762703cdd592624b31730400855ef3cefa9e7
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-11-30 20:08:00 +0200

    Remove old files

--- cypress/plugins/parcel.js
+++
@@ -1,9 +0,0 @@
-import { Parcel } from '@parcel/core';
-
-export const setupParcel = options => {
-  let bundler = new Parcel({
-    entries: ['cypress/compnent/***/*.cy.tsx'],
-  });
-
-  return bundler.watch();
-};
