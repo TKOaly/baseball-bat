fd070c9120b3ba772418bccefb60c2cf1b90ea60
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-02-20 16:37:46 +0200

    Add test for paying a debt with cash

--- packages/backend/tests/services/debts.test.ts
+++ packages/backend/tests/services/debts.test.ts
@@ -5,6 +5,7 @@ import { createDebtCenter } from '../../src/services/debt-centers/definitions';
 import { createPayerProfileFromEmailIdentity } from '../../src/services/payers/definitions';
 import { emailIdentity, euro } from '@bbat/common/src/types';
 import { getEmails } from '../../src/services/email/definitions';
+import { createPaymentEvent } from '@/services/payments/definitions';
 
 setup('Debts service', ({ test }) => {
   test('debt creation', async ({ bus }) => {
@@ -77,6 +78,7 @@ setup('Debts service', ({ test }) => {
     assert.ok(newDebt);
 
     assert.equal(newDebt.id, debt.id);
+    assert.equal(newDebt.status, 'unpaid');
     assert.notEqual(newDebt.publishedAt, null);
     assert.notEqual(newDebt.createdAt, null);
     assert.ok(!newDebt.draft);
@@ -133,6 +135,7 @@ setup('Debts service', ({ test }) => {
     assert.ok(newDebt);
 
     assert.equal(newDebt.id, debt.id);
+    assert.equal(newDebt.status, 'unpaid');
     assert.notEqual(newDebt.publishedAt, null);
     assert.notEqual(newDebt.createdAt, null);
     assert.ok(!newDebt.draft);
@@ -142,5 +145,70 @@ setup('Debts service', ({ test }) => {
 
     assert.equal(emails.length, 1);
     // FIXME: Should send credited email!
+  });
+
+  test('paying debt', async ({ bus }) => {
+    const payer = await bus.exec(createPayerProfileFromEmailIdentity, {
+      id: emailIdentity('test@test.test'),
+      name: 'Teppo Testaaja',
+    });
+
+    assert.ok(payer);
+
+    const center = await bus.exec(createDebtCenter, {
+      name: 'Test Center',
+      accountingPeriod: 2024,
+      description: 'Desc',
+      url: 'https://google.com/',
+    });
+
+    const component = await bus.exec(defs.createDebtComponent, {
+      debtCenterId: center.id,
+      name: 'Test Component',
+      amount: euro(10),
+      description: 'Test',
+    });
+
+    const debt = await bus.exec(defs.createDebt, {
+      debt: {
+        name: 'Name',
+        description: 'Desc',
+        centerId: center.id,
+        accountingPeriod: 2024 as any,
+        components: [component.id],
+        payer: payer.id,
+        paymentCondition: 14,
+        tags: [],
+      },
+    });
+
+    await bus.exec(defs.publishDebt, debt.id);
+
+    const payment = await bus.exec(defs.createPayment, {
+      debts: [debt.id],
+      payment: {
+        type: 'cash',
+        title: 'Title',
+        message: 'Desc.',
+      },
+    });
+
+    await bus.exec(createPaymentEvent, {
+      paymentId: payment.id,
+      type: 'payment',
+      amount: euro(10),
+      transaction: null,
+    });
+
+    const newDebt = await bus.exec(defs.getDebt, debt.id);
+
+    assert.ok(newDebt);
+
+    assert.equal(newDebt.id, debt.id);
+    assert.notEqual(newDebt.publishedAt, null);
+    assert.notEqual(newDebt.createdAt, null);
+    assert.ok(!newDebt.draft);
+    assert.ok(!newDebt.credited);
+    assert.equal(newDebt.status, 'paid');
   });
 });
