b8aca03953ca939019a76da7d1259cb786bed496
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-02-29 01:46:32 +0200

    Use same server setup code in testing and prod/dev

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,15 +1,7 @@
-import express from 'express';
-import { router } from 'typera-express';
-import healthCheck from './api/health-check';
 import 'reflect-metadata';
-import { RedisClientType } from 'redis';
 import { Config } from './config';
-import apiRoutes, { ApiDeps } from './api';
-import cookieParser from 'cookie-parser';
 import Stripe from 'stripe';
 import { Pool, Connection } from './db/connection';
-import cors from 'cors';
-import helmet, { HelmetOptions } from 'helmet';
 import * as redis from 'redis';
 import {
   createEmailDispatcherTransport,
@@ -18,38 +10,10 @@ import {
 } from './modules/email';
 import { JobService } from './modules/jobs';
 import { LocalBus } from './bus';
-import initServices from './modules';
+import server from './server';
 
 const PORT = process.env.PORT ?? '5000';
 const config = Config.get();
-
-const helmetConfig: HelmetOptions = {
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'", '*.stripe.com'],
-      scriptSrc:
-        process.env.NODE_ENV !== 'production'
-          ? [
-              "'self'",
-              '*.stripe.com',
-              "'unsafe-eval'",
-              'ws://bbat.tko-aly.localhost:1234',
-              'ws://localhost:1234',
-            ]
-          : ["'self'", '*.stripe.com'],
-      connectSrc:
-        process.env.NODE_ENV !== 'production'
-          ? [
-              "'self'",
-              'ws://bbat.tko-aly.localhost:1234',
-              'ws://localhost:1234',
-            ]
-          : ["'self'"],
-      frameAncestors: ['*.stripe.com'],
-    },
-  },
-  crossOriginEmbedderPolicy: false,
-};
 
 const stripeClient = new Stripe(config.stripeSecretKey, {
   apiVersion: '2020-08-27',
@@ -77,8 +41,6 @@ export type BusContext = {
 
 const bus = new LocalBus<BusContext>();
 
-// bus.on(shutdown, () => redisClient.shutdown());
-
 const jobs = new JobService(config, bus, pool);
 
 const moduleDeps = {
@@ -100,48 +62,8 @@ declare global {
   }
 }
 
-const app = express()
-  .use((req, _res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  })
-  .use(helmet(helmetConfig))
-  .use(
-    cors({
-      methods: ['GET', 'POST', 'OPTIONS'],
-      origin: [config.appUrl],
-    }),
-  )
-  .use(
-    express.json({
-      verify: (req, _res, buf) => {
-        (req as Express.Request).rawBody = buf;
-      },
-    }),
-  )
-  .use(cookieParser())
-  .use(router(healthCheck).handler());
-
-const apiDeps: ApiDeps = {
-  bus,
-  config,
-  redis: redisClient as RedisClientType,
-  pool,
-  jobs,
-};
-
-apiRoutes(moduleDeps, apiDeps, app);
-
-if (process.env.NODE_ENV !== 'production') {
-  app.use('/static', express.static('/usr/src/app/packages/frontend/dist'));
-  app.use('/', (_req, res) =>
-    res.sendFile('/usr/src/app/packages/frontend/dist/index.html'),
-  );
-}
-
 async function start() {
-  await initServices(app, moduleDeps);
-
+  const app = await server(moduleDeps);
   app.listen(PORT, () => console.log(`backend listening on port ${PORT} 🚀`));
 }
 
