239339983a8204171810b1e48d01dbcb21efabe7
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-03-03 19:51:08 +0200

    Use the PayementList component in the payment listing view

--- packages/frontend/src/views/admin/payments-listing.tsx
+++ packages/frontend/src/views/admin/payments-listing.tsx
@@ -1,54 +1,13 @@
-import { Table } from '@bbat/ui/table';
 import { useGetPaymentsQuery } from '../../api/payments';
-import { useLocation } from 'wouter';
-import { useHistoryPersister } from '../../hooks/use-history-persister';
-import { formatEuro } from '@bbat/common/src/currency';
+import { PaymentList } from '../../components/payment-list';
 
 export const PaymentsListing = () => {
   const { data: payments } = useGetPaymentsQuery();
-  const [, setLocation] = useLocation();
-  const historyPersiter = useHistoryPersister();
 
   return (
     <>
       <h1 className="text-2xl mt-10 mb-5">Payments</h1>
-
-      <Table
-        selectable
-        persist={historyPersiter('payments')}
-        rows={(payments ?? []).map(p => ({ ...p, key: p.id })) ?? []}
-        onRowClick={row => setLocation(`/admin/payments/${row.id}`)}
-        columns={[
-          {
-            getValue: row => row.paymentNumber,
-            name: 'No.',
-          },
-          {
-            getValue: row => row.type,
-            name: 'Type',
-          },
-          {
-            getValue: row => row.title,
-            name: 'Name',
-          },
-          {
-            getValue: row => {
-              if (row.credited) {
-                return 'Credited';
-              }
-
-              return row.status;
-            },
-            name: 'Status',
-          },
-          {
-            name: 'Balance',
-            align: 'right',
-            getValue: row => row.balance,
-            render: formatEuro,
-          },
-        ]}
-      />
+      <PaymentList payments={payments ?? []} />
     </>
   );
 };
