1542d23f5355a2729f91b7f55946127757080eaa
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-09-09 15:42:03 +0300

    Add seed file

---
+++ packages/backend/seed/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO payer_profiles (id, name) VALUES ('b3672629-35d0-48d2-8485-80daf3e4a9c5'::uuid, 'Admin');
+INSERT INTO payer_emails (payer_id, email) VALUES ('b3672629-35d0-48d2-8485-80daf3e4a9c5'::uuid, 'admin@admin.test');
