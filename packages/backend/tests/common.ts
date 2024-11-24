90c2b58098a23f708e578c80530ca52142bd7d61
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-11-24 11:16:31 +0200

    Configure logging when running tests

--- packages/backend/tests/common.ts
+++ packages/backend/tests/common.ts
@@ -165,7 +165,20 @@ export class Environment {
 
         return nats;
       },
-      logger: async () => winston.createLogger(),
+      logger: async () =>
+        winston.createLogger({
+          transports: [
+            new winston.transports.Console({
+              format: winston.format.combine(
+                winston.format.colorize({ level: true }),
+                winston.format.printf(info => {
+                  const module = info.module ? `[${info.module}] ` : '';
+                  return `[${info.level}] ${module}${info.message}`;
+                }),
+              ),
+            }),
+          ],
+        }),
     };
 
   deps: Partial<Deps> = {};
