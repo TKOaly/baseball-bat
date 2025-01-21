475678913ff7198d2eaeb6c05f322b0386666e88
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2025-01-21 15:30:11 +0200

    Expose the end date of the latest bank statement via the integration API

--- packages/backend/src/modules/integration/api.ts
+++ packages/backend/src/modules/integration/api.ts
@@ -2,6 +2,11 @@ import { RouterFactory } from '@/module';
 import { Parser, Response, Middleware, router } from 'typera-express';
 import * as debtService from '@/modules/debts/definitions';
 import * as payerService from '@/modules/payers/definitions';
+import * as bankingService from '@/modules/banking/definitions';
+import * as A from 'fp-ts/Array';
+import * as T from 'fp-ts/Task';
+import * as D from 'fp-ts/Date';
+import * as O from 'fp-ts/Option';
 import {
   internalServerError,
   notFound,
@@ -9,6 +14,7 @@ import {
   unauthorized,
 } from 'typera-express/response';
 import { paginationQuery } from '@bbat/common/types';
+import { flow } from 'fp-ts/function';
 
 type AuthServiceMiddleware = Middleware.ChainedMiddleware<
   { isServiceRequest: boolean },
@@ -65,7 +71,37 @@ const routes: RouterFactory = route => {
       });
     });
 
-  return router(getDebtsByMembersId, getDebt);
+  const getBankingInfo = route
+    .get('/banking-info')
+    .use(authService)
+    .handler(({ bus }) =>
+      flow(
+        bus.execT(bankingService.getBankAccounts),
+        T.chain(
+          A.traverse(T.ApplicativePar)(
+            flow(
+              ({ iban }) => iban,
+              bus.execT(bankingService.getAccountStatements),
+            ),
+          ),
+        ),
+        T.map(
+          flow(
+            A.flatten,
+            A.map(statement => statement.closingBalance.date),
+            A.sort(D.Ord),
+            A.last,
+            O.toNullable,
+            latest =>
+              ok({
+                latestBankInfo: latest,
+              }),
+          ),
+        ),
+      )()(),
+    );
+
+  return router(getDebtsByMembersId, getDebt, getBankingInfo);
 };
 
 export default routes;
